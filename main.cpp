aee51ad1f4d01609a0e20339b543ace0ad8ec10b
MizzenMastodon <Nbaum0701@gmail.com>
2024-10-01 00:48:24 +0000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -9,14 +9,22 @@ int main() {
   uniform_int_distribution<int> randomInt(0,4999);
   int num1 = randomInt (myEngine);
   int num2 = randomInt (myEngine);
-
+  //creates a variable for the student's answer
+  int studentAnswer;
   //uses the two random numbers to create an addition problem
+  cout << "Press enter after you have given your answer to check your work\n\n";
+
   cout << setw(6) << num1 << endl;
   cout << "+" << setw(5) << num2 << endl;
-  cout << "------";
-  cout << "    Press enter after you have answered the problem to check your work";
-  cin.get();
+  cout << "------\n  ";
   
-  cout << setw(6) << num1 + num2;
-  
+  cin >> studentAnswer;
+
+  if (studentAnswer == (num1 + num2)) {
+    cout << "You are correct!";
+  }
+  else {
+    cout << "Your answer is not correct.\n";
+    cout << "The correct answer is " << num1 + num2;
+  }
 }
